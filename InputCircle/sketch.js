a68a0e159a30805b7c188382b61b57c1fe0904c4
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-03-03 04:16:20 +0100

    Added effects and OSC functionality

--- InputCircle/sketch.js
+++ InputCircle/sketch.js
@@ -1,15 +1,25 @@
+var socket;
+
 var circles = new Array();
-var colors = new Array('rgb(255,255,255)', "rgb(120,120,120)", "rgb(10,10,10)", "rgb(255,75,56)");
+var colors = new Array('rgba(255,255,255,1.0)', "rgba(120,120,120,1.0)", "rgba(10,10,10,1.0)", "rgba(255,75,56,1.0)");
 var fillColors = new Array('rgba(255,255,255, 0.2)', "rgba(120,120,120,0.2)", "rgba(10,10,10,0.2)", "rgba(255,75,56,0.5)");
 var mouseXPressed = 0;
 var mouseYPressed = 0;
 var nextColorIndex = 0;
 var currentCircle;
 
+
 function setup() {
   createCanvas(1000, 1000);
   noFill();
   //blendMode(LIGHTEST);
+
+  socket = io('http://Sbhklr-MacBook-Pro.local:3000');  
+  socket.on('connect', function() { print(" Connected. "); });
+}
+
+function sendOsc(address, value) {
+  socket.emit('message', [address].concat(value));
 }
 
 function draw() {
@@ -25,7 +35,6 @@ function draw() {
   for (var i = 0; i < circles.length; ++i) {
     drawCircle(circles[i]);
   }
-
 }
 
 function mousePressed() {
@@ -46,9 +55,8 @@ function mouseReleased() {
   }
   
  for (var i = 0; i < circles.length; ++i){
-   var circle = circles[i];
+  var circle = circles[i];
   sendOsc("/circles", i+1 + ";" + circle.x + ";" + circle.y +";" + circle.radius);
-  
  }
   
   
@@ -65,6 +73,8 @@ function createCircle(color, fillColor) {
   circle.y = mouseYPressed;
   circle.color = color;
   circle.fillColor = fillColor;
+  circle.currentExpand = 0.01;
+  circle.currentExpandDirection = 1.0;
   return circle;
 }
 
@@ -72,9 +82,24 @@ function mouseDistanceToCenter() {
   return int(dist(width / 2, height / 2, mouseX, mouseY));
 }
 
+function rgbColor(color){
+  return "rgba(" + color.r + "," + color.g + "," + color.b + "," + color.a + ")";
+}
+
 function drawCircle(circle) {
+
+  var maximumExpand = circle.radius / 10.0;
+  console.log(circle.currentExpand / maximumExpand);
+  var expandStep = (maximumExpand / 8.0) * circle.currentExpandDirection;
+
+  circle.currentExpand = circle.currentExpand + expandStep;
+  if(circle.currentExpand > maximumExpand || circle.currentExpand < -maximumExpand){
+     circle.currentExpandDirection *= -1;
+     //circle.currentExpand = 0;
+  }
+
   fill(color(circle.fillColor));
   stroke(color(circle.color));
   strokeWeight(1);
-  ellipse(circle.x, circle.y, circle.radius, circle.radius);
+  ellipse(circle.x, circle.y, circle.radius + circle.currentExpand, circle.radius + circle.currentExpand);
 }
